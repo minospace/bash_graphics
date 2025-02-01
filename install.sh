b2e3b868b7fd952dafe56b7c866d9d2c2f32a129
minospace <119105620+minospace@users.noreply.github.com>
2025-02-01 16:58:15 +0100

    removed comments that were not necessary

--- install.sh
+++ install.sh
@@ -5,22 +5,17 @@ echo "this script will install bash graphics"
 echo "you can stop this script by pressing ctrl+c"
 read -p "press enter to continue or ctrl+c to stop"
 
-# URL van het gecomprimeerde bestand
 url="https://drive.google.com/uc?id=1t2lxhD7fXKa2gLN-sURc5zENDod-DB66"
 
-# Specifieke directory waar je het bestand wilt uitpakken
 directory="/Users/$USER/coding"
 
-# Maak de directory als die nog niet bestaat
 echo -e "checking if directory $directory exists"
 echo -e "if not, creating directory $directory"
 mkdir -p "$directory"
 
-# Download het bestand met curl
 echo -e "downloading bash graphics"
 curl -L -o "$directory/bash_graphics.zip" "$url"
 
-# Pak het gedownloade bestand uit
 echo -e "unzipping bash graphics"
 unzip "$directory/bash_graphics.zip" -d "$directory"
 
